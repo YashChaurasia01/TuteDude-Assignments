2a23c48494c8273b30752ae105f8e23a6ba184a2
YashChaurasia01 <yashbhai9876@gmail.com>
2025-12-03 13:02:45 +0530

    Changes Saved

---
+++ Assignment 1/tuteDA1Q1.py
@@ -0,0 +1,19 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(f"\nAddition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+
+"""
+Code Output:
+
+Enter the first number: 5
+Enter the second number: 10
+
+Addition: 15
+Subtraction: -5
+Multiplication: 50
+Division: 0.5
+"""
---
+++ Assignment 1/tuteDA1Q2.py
@@ -0,0 +1,13 @@
+f_name = input("Enter your first name: ")
+l_name = input("Enter your second name: ")
+
+print(f"\nHello, {f_name.title() + " " + l_name.title()}! Welcome to the Python program.")
+
+"""
+Code Output:
+
+Enter your first name: Yash 
+Enter your second name: Chaurasia
+
+Hello, Yash Chaurasia! Welcome to the Python program.
+"""
